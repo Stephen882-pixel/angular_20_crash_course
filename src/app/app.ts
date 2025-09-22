44affe841d8c63a5160aecf6467185fe4c2da459
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-22 22:21:42 +0300

    components

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Admin} from './components/admin/admin';
+import {User} from './components/user/user';
+
+@Component({
+  selector: 'app-root',
+  imports: [Admin,User],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular_20_tutorial');
+}
---
+++ src/app/components/admin/admin.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css'
+})
+export class Admin {
+
+  firstName = "Stephen";
+
+  courseName = "Bsc Computer Science";
+
+  rollNo = 111;
+
+}
