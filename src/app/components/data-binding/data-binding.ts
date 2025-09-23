8c46d71ba9de130c0212df4d5da401de09571149
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-23 11:30:23 +0300

    data binding

--- src/app/components/data-binding/data-binding.ts
+++ src/app/components/data-binding/data-binding.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css'
 })
 export class DataBinding {
+    courseName: string = "Angular_20_tutorial";
+    productPrice: number = 12500;
+    maxlength : number = 5;
+    minChar :number = 3;
+    inputType :string = "checkbox";
+    myClassName :string = "myColor";
 
+    constructor() {
+
+    }
+
+    showWelcomeMsg() {
+      alert("Welcome to Angular 20 Tutorial");
+    }
+    changeCourseName() {
+      this.courseName = "Angular_20_tutorial - Updated";
+    }
+    onCityChange(){
+      alert("City Changed");
+    }
 }
